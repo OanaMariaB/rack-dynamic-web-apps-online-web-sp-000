3fa5decde166012c9a63db42a95134080b269b9c
Oana Berentan <oana.berentan@yahoo.com>
2019-10-08 19:47:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
-
+ 
   def call(env)
     resp = Rack::Response.new
+<<<<<<< HEAD
 
     num_1 = Kernel.rand(1..2)
     num_2 = Kernel.rand(1..2)
@@ -11,13 +12,28 @@ class Application
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
 
+=======
+ 
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+ 
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+ 
+>>>>>>> 92d6b0ee86cc3e1ed80b8b1ac18e1e94b57ffade
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
       resp.write "You Lose"
     end
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> 92d6b0ee86cc3e1ed80b8b1ac18e1e94b57ffade
     resp.finish
   end
-
+ 
 end
